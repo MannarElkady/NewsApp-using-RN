3fed1647d48ee796b4abe6d3362c18319f3f4904
MannarElkady <manaraelkady@gmail.com>
2022-04-01 02:39:23 +0200

    add navigation layer separated

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import NewsDetails from '../Component/NewsDetails';
+import TabNavigator from '../Component/TabNavigator';
 
 const Navigation = () => {
   const MainStack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <MainStack.Navigator>
-        <MainStack.Screen name="Details Page" component={NewsDetails} />
+        <MainStack.Screen
+          name="TabNavigator"
+          component={TabNavigator}
+          options={{headerShown: false}}
+        />
+        <MainStack.Screen name="DetailsPage" component={NewsDetails} />
       </MainStack.Navigator>
     </NavigationContainer>
   );
