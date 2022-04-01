12417dbc89c72ceb720f3d21de5c252bb8780634
MannarElkady <manaraelkady@gmail.com>
2022-04-01 22:45:32 +0200

    update useGetNews logic

--- src/Screens/Home/useGetNews.ts
+++ src/Screens/Home/useGetNews.ts
@@ -1,18 +1,30 @@
 import {useMemo} from 'react';
+import {setNews} from '../../Redux/Slicers/NewsListReducer';
+import {useAppDispatch} from '../../Redux/store';
 import {AxiosClient, useGetHandler} from '../../Service';
 import {DomainConstants} from '../../Types';
 
 const useGetNews = () => {
+  let dispatch = useAppDispatch();
+  //save news data to redux
+  const successCallback = (data: any) => {
+    dispatch(setNews(data.articles));
+  };
+
   let baseURL = DomainConstants.baseURL;
   var client = useMemo(() => {
     let params = {
-      domains: 'wsj.com',
-      apiKey: 'ee54174d1e7247639fd4ef02e93634bd',
+      domains: DomainConstants.domains,
+      apiKey: DomainConstants.apiKey,
     };
     return AxiosClient(baseURL, params);
   }, [baseURL]);
-  const [data, isLoading] = useGetHandler(DomainConstants.postsURL, client);
-  return [data, isLoading];
+  const [, status] = useGetHandler(
+    DomainConstants.postsURL,
+    client,
+    successCallback,
+  );
+  return [status];
 };
 
 export default useGetNews;
