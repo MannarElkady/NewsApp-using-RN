4559ff6c4a6043c04ebab6b9667e4da92192ad7e
MannarElkady <manaraelkady@gmail.com>
2022-04-01 02:38:35 +0200

    update item component to navigate to details page

--- src/Component/Item/index.tsx
+++ src/Component/Item/index.tsx
@@ -2,17 +2,28 @@ import {Image, TouchableWithoutFeedback, Text, View} from 'react-native';
 import React from 'react';
 import {NewItemTileModel} from '../../Types';
 import styles from './style';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   item: NewItemTileModel;
 }
 
 const Item = ({item}: Props) => {
+  let navigation = useNavigation();
   return (
-    <View style={styles.itemContainer}>
-      <Image source={{uri: item.urlToImage}} style={styles.imageContainer} />
-      <Text style={styles.textstyle}>{item.title}</Text>
-    </View>
+    <TouchableWithoutFeedback
+      key={item.index}
+      onPress={() => {
+        navigation.navigate('DetailsPage', item.item);
+      }}>
+      <View style={styles.itemContainer}>
+        <Image
+          source={{uri: item.item.urlToImage}}
+          style={styles.imageContainer}
+        />
+        <Text style={styles.textstyle}>{item.item.title}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
