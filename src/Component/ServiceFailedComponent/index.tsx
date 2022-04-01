8f1d8743c6da8246dcae64633fdb4cc0fef607ad
MannarElkady <manaraelkady@gmail.com>
2022-04-01 22:39:06 +0200

    add basic service failed component

--- src/Component/ServiceFailedComponent/index.tsx
+++ src/Component/ServiceFailedComponent/index.tsx
@@ -1,14 +1,17 @@
 import {StyleSheet, Text} from 'react-native';
 import React from 'react';
 
+//TODO: Enhance UI
 const ServiceFailedComponent = () => {
   return (
-    <Text style={{fontSize: 21, flex: 1, justifyContent: 'center'}}>
-      Service Failed
+    <Text style={styles.container}>
+      Service Failed Service Failed Service FailedService Failed
     </Text>
   );
 };
 
 export default ServiceFailedComponent;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {fontSize: 21, flex: 1, justifyContent: 'center'},
+});
