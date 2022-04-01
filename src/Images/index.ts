1aebc42e532cc7be33b06ade18dfd39c21263208
MannarElkady <manaraelkady@gmail.com>
2022-04-01 22:42:42 +0200

    add seach bar assets

--- src/Images/index.ts
+++ src/Images/index.ts
@@ -1,6 +1,8 @@
 const Images = {
   newsFeedIcon: require('../Assets/home.png'),
   settingIcon: require('../Assets/setting.png'),
+  searchIcon: require('../Assets/searchIcon.png'),
+  crossImg: require('../Assets/crossImg.png'),
 };
 
 export default Images;
