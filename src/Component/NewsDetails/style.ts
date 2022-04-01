dee5b6e5d822009a07d41787b1f82f6ce9490590
MannarElkady <manaraelkady@gmail.com>
2022-04-01 02:41:48 +0200

    add detials page component style

---
+++ src/Component/NewsDetails/style.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: '100%',
+    height: '30%',
+    zIndex: 1,
+    elevation: 1,
+    resizeMode: 'stretch',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  textsContainer: {
+    width: '100%',
+    height: '70%',
+  },
+});
+
+export default styles;
