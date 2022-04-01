3c304cca787202d7d0abf018f37e0a70232ba546
MannarElkady <manaraelkady@gmail.com>
2022-04-01 02:41:56 +0200

    add details page component

--- src/Component/NewsDetails/index.tsx
+++ src/Component/NewsDetails/index.tsx
@@ -1,14 +1,22 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import React from 'react';
+import {NewItemTileModel} from '../../Types';
+import styles from './style';
 
-const NewsDetails = () => {
+const NewsDetails = ({route}) => {
+  let item: NewItemTileModel = route.params;
   return (
     <View>
-      <Text>index</Text>
+      <Image style={styles.imageContainer} source={{uri: item.urlToImage}} />
+      <View style={styles.textsContainer}>
+        <Text>{item.title}</Text>
+        <Text>{item.author}</Text>
+        <Text>{item.publishedAt}</Text>
+        <Text>{item.content}</Text>
+        <Text>{item.description}</Text>
+      </View>
     </View>
   );
 };
 
 export default NewsDetails;
-
-const styles = StyleSheet.create({});
