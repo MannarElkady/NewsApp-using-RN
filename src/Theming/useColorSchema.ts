c4fd452a5c39449c05ba981ac3e59e62484e8663
MannarElkady <manaraelkady@gmail.com>
2022-04-04 20:24:45 +0200

    move theme directory to main src folder

---
+++ src/Theming/useColorSchema.ts
@@ -0,0 +1,36 @@
+import {Appearance, ColorSchemeName} from 'react-native';
+import {useEffect, useRef, useState} from 'react';
+
+export default function useColorScheme(
+  delay = 500,
+): NonNullable<ColorSchemeName> {
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+
+  let timeout = useRef<NodeJS.Timeout | null>(null).current;
+
+  useEffect(() => {
+    Appearance.addChangeListener(onColorSchemeChange);
+
+    return () => {
+      resetCurrentTimeout();
+      Appearance.removeChangeListener(onColorSchemeChange);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function onColorSchemeChange(preferences: Appearance.AppearancePreferences) {
+    resetCurrentTimeout();
+
+    timeout = setTimeout(() => {
+      setColorScheme(preferences.colorScheme);
+    }, delay);
+  }
+
+  function resetCurrentTimeout() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+  }
+
+  return colorScheme as NonNullable<ColorSchemeName>;
+}
