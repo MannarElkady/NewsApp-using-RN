59e0a7301a46d0dbad677e100254571af84ba7ab
MannarElkady <manaraelkady@gmail.com>
2022-04-04 22:38:08 +0200

    add trnaslation slicer

---
+++ src/Redux/Slicers/LanguageReducer.ts
@@ -0,0 +1,17 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+const initialState = {
+  language: 'en',
+};
+const LanguageSlice = createSlice({
+  name: 'language',
+  initialState: initialState,
+  reducers: {
+    setLanguage: (state, action: PayloadAction<string>) => {
+      state.language = action.payload;
+    },
+  },
+});
+
+export const {setLanguage} = LanguageSlice.actions
+export default LanguageSlice.reducer;
--- src/Redux/Slicers/index.ts
+++ src/Redux/Slicers/index.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from '@reduxjs/toolkit';
+import LanguageReducer from './LanguageReducer';
 import NewsListReducer from './NewsListReducer';
 import ThemingReducer from './ThemingReducer';
 
 const rootReducer = combineReducers({
   newsFeedReducer: NewsListReducer,
   themingReducer: ThemingReducer,
+  languageReducer: LanguageReducer,
 });
 
 export default rootReducer;
